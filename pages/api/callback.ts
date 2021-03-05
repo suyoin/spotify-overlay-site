13091920d1215131f2d398bc54461381c70c6e50
suyoin <19378617+suyoin@users.noreply.github.com>
2021-03-05 12:55:30 -0600

    Update callback.ts

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -13,7 +13,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const response = await axios({
 		method: "POST",
 		url: "https://accounts.spotify.com/api/token",
-		params: {
+		data: {
 			code: req.query.code,
 			redirect_uri: "https://spotify-overlay-site.vercel.app",
 			grant_type: "authorization_code",
